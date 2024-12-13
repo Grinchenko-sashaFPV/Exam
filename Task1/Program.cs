de4b135c0431ee9cf8e07db7cffd0b7665fc78f2
oleksandr.biloshytskyi <oleksandr.biloshytskyi@qpm.ai>
2024-12-13 15:22:57 +0200

    feat: tasks for exam

---
+++ Task1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Security.Cryptography;
+
+const string inputFilePath = "../../../Files/input.txt";
+const string encryptedFilePath = "../../../EncryptedFiles/encrypted.bin";
+
+// Key & init vector (IV) for AES-GCM
+byte[] key = new byte[32]; // 256-bit key
+byte[] iv = new byte[12]; // 96-bit IV
+RandomNumberGenerator.Fill(key);
+RandomNumberGenerator.Fill(iv);
+
+try
+{
+    string plaintext = File.ReadAllText(inputFilePath);
+    byte[] plaintextBytes = System.Text.Encoding.UTF8.GetBytes(plaintext);
+
+    // Буфери для шифротексту і тегу автентифікації
+    byte[] ciphertext = new byte[plaintextBytes.Length];
+    byte[] tag = new byte[16];
+
+    using (AesGcm aes = new(key))
+    {
+        aes.Encrypt(iv, plaintextBytes, ciphertext, tag);
+    }
+
+    WriteEncryptedFile(ciphertext);
+
+    // Display result
+    Console.WriteLine("Encryption completed.");
+    Console.WriteLine("Ciphertext written to file: " + encryptedFilePath);
+    Console.WriteLine("IV: " + Convert.ToBase64String(iv));
+    Console.WriteLine("Key: " + Convert.ToBase64String(key));
+    Console.WriteLine("Authentication tag: " + Convert.ToBase64String(tag));
+
+    // Decryption
+    Console.WriteLine("\n--- Decryption ---");
+    Console.Write("Pls, provide IV (Base64): ");
+    iv = Convert.FromBase64String(Console.ReadLine());
+
+    Console.Write("Provide key (Base64): ");
+    key = Convert.FromBase64String(Console.ReadLine());
+
+    Console.Write("And authentication tag (Base64): ");
+    tag = Convert.FromBase64String(Console.ReadLine());
+
+    byte[] decryptedPlaintext = new byte[ciphertext.Length];
+
+    using (AesGcm aes = new AesGcm(key))
+    {
+        aes.Decrypt(iv, ciphertext, tag, decryptedPlaintext);
+    }
+
+    string decryptedText = System.Text.Encoding.UTF8.GetString(decryptedPlaintext);
+    DisplayDecryptedText(decryptedText);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Exception: " + ex.Message);
+} 
+
+void WriteEncryptedFile(byte[] ciphertext)
+{
+    using (FileStream fs = new FileStream(encryptedFilePath, FileMode.Create, FileAccess.Write))
+    {
+        fs.Write(ciphertext);
+    }
+}
+
+void DisplayDecryptedText(string text)
+{
+    Console.WriteLine("Decrypted text: ");
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine(text);
+    Console.ForegroundColor = ConsoleColor.White;
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+const int port = 443;
+string response = "y";
+while (response == "y")
+{
+    Console.Write("Enter the server address (example: google.com): ");
+    string server = Console.ReadLine();
+
+    try
+    {
+        using (TcpClient client = new TcpClient(server, port))
+        using (SslStream sslStream = new SslStream(client.GetStream(), false, ValidateServerCertificate))
+        {
+            sslStream.AuthenticateAsClient(server);
+
+            Console.WriteLine("TLS connection established successfully.");
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Error: " + ex.Message);
+    }
+
+    Console.Write("Do you want to check another server? (y/n): ");
+    response = Console.ReadLine().ToLower();
+}
+
+static bool ValidateServerCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+{
+    if (sslPolicyErrors == SslPolicyErrors.None)
+    {
+        Console.WriteLine("The certificate is valid.");
+    }
+    else
+    {
+        Console.WriteLine("Certificate errors: " + sslPolicyErrors);
+        return false;
+    }
+
+    var x509Cert = new X509Certificate2(certificate);
+    byte[] publicKeyHash = SHA256.HashData(x509Cert.GetPublicKey());
+
+    Console.WriteLine("Public key hash (SHA-256): " + BitConverter.ToString(publicKeyHash).Replace("-", ""));
+
+    return true;
+}
